df9572d4684a238b9c0aea7c8b87f39fda6a7298
Balint Pataki <balint.pataki.automation@gmail.com>
2024-04-05 18:19:27 +0200

    implemented api call

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::fs;
 use std::process::exit;
+use std::error::Error;
 use toml;
 use serde::Deserialize;
+use reqwest;
 
 #[derive(Debug, Deserialize)]
 struct CityConfig {
@@ -22,21 +24,49 @@ struct Config {
 	weather_parameters: WeatherParameters,
 }
 
-fn main() {
-	let filename = "src/config.toml";
-	let contents = match fs::read_to_string(filename) {
-        // If successful return the files text as `contents`.
-        // `c` is a local variable.
+#[derive(Debug, Deserialize)]
+struct HourlyWeather {
+	//time: Vec<String>,
+	temperature_2m: Vec<f32>,
+	//precipitation_probability: Vec<i8>,
+	precipitation: Vec<f32>,
+//	rain: Vec<f32>,
+//	snow: Vec<f32>,
+//	cloud_cover: Vec<i8>,
+//	windspeed_10m: Vec<f32>,
+//	winddirection_10m: Vec<i8>,
+//	weather_code: Vec<i8>,
+}
+
+#[derive(Debug, Deserialize)]
+struct Weather {
+	hourly: HourlyWeather,
+}
+
+fn get_config(filename: String) -> Config {
+	let contents = match fs::read_to_string(filename.clone()) {
         Ok(c) => c,
-        // Handle the `error` case.
         Err(_) => {
-            // Write `msg` to `stderr`.
             eprintln!("Could not read file `{}`", filename);
-            // Exit the program with exit code `1`.
             exit(1);
         }
     };
 	let config: Config = toml::from_str(&contents).expect("Failed to read toml config!");
-	println!("{:#?}", config);
-	println!("{}", config.city[2].name);
+	return config;
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+	let config = get_config("src/config.toml".to_string());
+	let url = format!("https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly={params}&timezone=auto&forecast_days={forecast_days}",
+		lat = config.city[0].coordinates[0],
+		lon = config.city[0].coordinates[1],
+		params = "temperature_2m,precipitation",
+		forecast_days = 3
+	);
+	println!("{}" ,url);
+	let response = reqwest::get(&url).await?;
+	let weather = response.json::<Weather>().await?;
+	println!("{} mm", weather.hourly.precipitation[0]);
+	Ok(())
 }
